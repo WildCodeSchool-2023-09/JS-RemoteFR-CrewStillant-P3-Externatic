9c4896cc1f6a43985f6baec0ee88649522968155
chikoulat <hamidouboum@hotmail.fr>
2024-01-11 14:43:26 +0100

    FEAT: table user and message created. FEAT: joined the tables. UPDATE: updated candidate component, recovered data from backend.

--- frontend/src/components/user/UserProfil.jsx
+++ frontend/src/components/user/UserProfil.jsx
@@ -7,33 +7,31 @@ function UserProfil() {
   return (
     <div className="profile-section">
       <div className="user-image">
-        {user &&
-          user.map((u) => (
-            <img
-              src={u.image}
-              alt="candidate"
-              className="profile-pic"
-              key={u.id}
-            />
-          ))}
+        {user.map((u) => (
+          <img
+            src={u.image}
+            alt="candidate"
+            className="profile-pic"
+            key={u.id}
+          />
+        ))}
 
         <button type="button" className="button-space">
           Changer votre photo
         </button>
       </div>
       <div className="user-info">
-        {user &&
-          user.map((u) => (
-            <div key={u.id}>
-              <p> Nom: {u.lastname} </p>
-              <p> Prénom: {u.firstname} </p>
-              <p> E-mail: {u.email} </p>
-              <p> Date de naissance: {u.date_of_birth} </p>
-              <p> Date d'inscription: {u.registration_date} </p>
-              <p> Téléphone: {u.contact_number} </p>
-              <p> Salaire souhaité: {u.wanted_salary} </p>
-            </div>
-          ))}
+        {user.map((u) => (
+          <div key={u.id}>
+            <p> Nom: {u.lastname} </p>
+            <p> Prénom: {u.firstname} </p>
+            <p> E-mail: {u.email} </p>
+            <p> Date de naissance: {u.date_of_birth} </p>
+            <p> Date d'inscription: {u.registration_date} </p>
+            <p> Téléphone: {u.contact_number} </p>
+            <p> Salaire souhaité: {u.wanted_salary} </p>
+          </div>
+        ))}
 
         <hr />
         <button type="button" className="button-space">
