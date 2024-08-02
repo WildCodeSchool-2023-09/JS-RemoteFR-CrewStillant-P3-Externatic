c2d5d52a24513340e0343a2646b1c1569da97e4e
chikoulat <hamidouboum@hotmail.fr>
2024-08-02 19:54:22 +0100

    FIX: Fixed favicon import link.

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
 
